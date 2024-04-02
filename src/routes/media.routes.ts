e21f7c03609bcd4f56188bbd3f574705fb4ca3c7
Tiep Pham <payvip1802@gmail.com>
2024-04-02 08:40:22 +0700

    verify media routes

--- src/routes/media.routes.ts
+++ src/routes/media.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { uploadImagesController, uploadVideosController } from '~/controllers/medias.controllers'
+import { accessTokenValidator, verifyUserValidator } from '~/middlewares/users.middlewares'
 import { wrapRequestHandler } from '~/utils/handler'
 const mediaRoute = Router()
 
@@ -10,7 +11,7 @@ const mediaRoute = Router()
  * Body: {images: images file}
  * Header: {'Content-Type': '}
  */
-mediaRoute.post('/upload/images', wrapRequestHandler(uploadImagesController))
+mediaRoute.post('/upload/images', accessTokenValidator, verifyUserValidator, wrapRequestHandler(uploadImagesController))
 
 /**
  * Description: Upload video
@@ -19,6 +20,6 @@ mediaRoute.post('/upload/images', wrapRequestHandler(uploadImagesController))
  * Body: {images: video file}
  * Header: {'Content-Type': '}
  */
-mediaRoute.post('/upload/videos', wrapRequestHandler(uploadVideosController))
+mediaRoute.post('/upload/videos', accessTokenValidator, verifyUserValidator, wrapRequestHandler(uploadVideosController))
 
 export default mediaRoute
